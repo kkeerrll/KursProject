aee762c04b3d8606a604706ee752db2c65369b83
anzelikagudkova <kkeerrll137@gmail.com>
2023-08-04 14:38:46 +0300

    total

--- json_saver.py
+++ json_saver.py
@@ -2,6 +2,8 @@ import json
 from abc import ABC, abstractmethod
 from api import HeadHunterAPI, SuperJobAPI, Api
 from vacancy import Vacancy
+import os
+
 
 class Abstract_action(ABC):
     def add_vacancy(self, vacancy: Vacancy):
@@ -13,19 +15,30 @@ class Abstract_action(ABC):
     def delete_vacancy(self, vacancy: Vacancy):
         pass
 
+    def delete_file(self, vacancy: Vacancy):
+        pass
+
 
 class JSONSaver(Abstract_action):
     """Класс работает с данными файла"""
     file_name: str = 'vacancies.json'
 
     @staticmethod
+    def delete_file():
+        if os.path.exists(JSONSaver.file_name):
+            os.remove(JSONSaver.file_name)
+
+
+    @staticmethod
     def get_vacancies():
-        with open(JSONSaver.file_name, 'r', encoding='utf-8') as file:
-            contents = file.read()
-            if contents == "":
-                return []
-            else:
-                return json.loads(contents)
+        with open(JSONSaver.file_name, 'a', encoding='utf-8') as file:
+            with open(JSONSaver.file_name, 'r', encoding='utf-8') as file:
+                contents = file.read()
+                if contents == "":
+                    return []
+                else:
+                    return json.loads(contents)
+
 
     def save(self, vacancies):
         data = json.dumps(
@@ -46,22 +59,20 @@ class JSONSaver(Abstract_action):
         vacancies.append(vacancy)
         self.save(vacancies)
 
-    def get_vacancies_by_salary(self, salary_range: str):
-        vacancies = self.get_vacancies()
-        salaries = salary_range.split('-')
-
-        if salaries != ['']:
-            min_value = int(''.join([letter for letter in salaries[0] if letter.isdigit()]) or 0)
-            max_value = int(''.join([letter for letter in salaries[-1] if letter.isdigit()]) or min_value)
-            if min_value == 0:
-                return int(max_value)
-            else:
-                return int((min_value + max_value) / 2)
-        else:
-            raise ValueError("Invalid salary format")
+    # def get_vacancies_by_salary(self, salary_range: str):
+    #     vacancies = self.get_vacancies()
+    #     salaries = salary_range.split('-')
+    #
+    #     if salaries != ['']:
+    #         min_value = int(''.join([letter for letter in salaries[0] if letter.isdigit()]) or 0)
+    #         max_value = int(''.join([letter for letter in salaries[-1] if letter.isdigit()]) or min_value)
+    #         if min_value == 0:
+    #             return int(max_value)
+    #         else:
+    #             return int((min_value + max_value) / 2)
+    #     else:
+    #         raise ValueError("Invalid salary format")
 
     def delete_vacancy(self, vacancy: Vacancy):
         vacancies = [v for v in self.get_vacancies() ]
-        # if v['url'] != vacancy.url]
-        # vacancies = [v for v in self.get_vacancies() if v.get('url') != vacancy.url]
         self.save(vacancies)
--- main.py
+++ main.py
@@ -2,37 +2,27 @@
 import sys
 from api import HeadHunterAPI, SuperJobAPI
 from json_saver import JSONSaver
-from vacancy import Vacancy
+import json
+import os
 
-hh_api = HeadHunterAPI()
-superjob_api = SuperJobAPI()
-
-# Получение вакансий с разных платформ
-# hh_vacancies = hh_api.get_vacancies("Python")
-# superjob_vacancies = superjob_api.get_vacancies("Python")
-
-# Создание экземпляра класса для работы с вакансиями
-vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000",  "руб", "Требования: опыт работы от 3 лет...")
-
-# Сохранение информации о вакансиях в файл
-# json_saver = JSONSaver()
-# json_saver.add_vacancy(vacancy)
-# json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-# json_saver.delete_vacancy(vacancy)
-
-# # Функция для взаимодействия с пользователем
-
+# Функция для взаимодействия с пользователем
+json_saver = JSONSaver()
 def user_interaction():
+    #Подготовка к началу работы
+    json_saver.delete_file()
+    if os.path.exists("total.txt"):
+        os.remove("total.txt")
+
     # Выбор платформы и запись данных в файл
     platforms = input("Выберете платформу для поиска работы:\n1 - HeadHunter, 2 - SuperJob\n")
     if platforms == '1':
+        hh_api = HeadHunterAPI()
         hh_vacancies = hh_api.get_vacancies("Python")
-        json_saver = JSONSaver()
         json_saver.add_vacancy(hh_vacancies)
         # json_saver.get_vacancies_by_salary("1-150 000 руб.")
     elif platforms == '2':
+        superjob_api = SuperJobAPI()
         superjob_vacancies = superjob_api.get_vacancies("Python")
-        json_saver = JSONSaver()
         json_saver.add_vacancy(superjob_vacancies)
     else:
         print("Не выбрана платформа, попробуйте еще раз")
@@ -47,43 +37,51 @@ def user_interaction():
     elif search_query == '2':
         currency = ['USD']
     elif search_query == '3':
-        currency.append([i['currency'] for i in json_get])
+        list = []
+        list.append([i['currency'] for i in json_get])
+        currency = [item for sublist in list for item in sublist]
     else:
         print("Не указана валюта, попробуйте еще раз")
         sys.exit()
 
     salary = int(input("Введите зарплату, которую хотите получать:\n"))
 
-    # top_n = int(input("Введите количество вакансий для вывода в топ N:\n"))
-    print(currency)
+
+    # for i in json_get:
+    #     if i['currency'] in currency and i['salary'] >= salary:
+    #         data = json.dumps(
+    #             i,
+    #             default=lambda o: o.__dict__,
+    #             sort_keys=True,
+    #             indent=4,
+    #             ensure_ascii=False
+    #         )
+    #         with open("total.txt", "a", encoding="utf-8") as file:
+    #             file.append(data)
     for i in json_get:
-        if i['currency'] in currency:
-            print(i.JSONSaver.get_vacancies_by_salary('1-150 000'))
+        if i['currency'] in currency and i['salary'] >= salary:
+            data = json.dumps(
+                i,
+                default=lambda o: o.__dict__,
+                sort_keys=True,
+                indent=4,
+                ensure_ascii=False
+            )
+            with open("total.txt", "a", encoding="utf-8") as file:
+                file.write(data)
+            print(i['name'])
 
-
-#     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-#     filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
-#
-#     if not filtered_vacancies:
-#         print("Нет вакансий, соответствующих заданным критериям.")
-#         return
-#
-#     sorted_vacancies = sort_vacancies(filtered_vacancies)
-#     top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-#     print_vacancies(top_vacancies)
-
-
-user_interaction()
-
-#
-# if __name__ == "__main__":
-#     user_interaction()
+    with open("total.txt", "a", encoding="utf-8") as file:
+        with open("total.txt", 'r', encoding='utf-8') as file:
+            contents = file.read()
+            if contents == "":
+                print("Нет вакансий, соответствующих заданным критериям.")
 
 
 
 
+if __name__ == "__main__":
+    user_interaction()
 
-# print(Superjob().api_response())
 
-# JSONSaver().save("pjjnkjnk.json")
 
